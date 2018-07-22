5f041f14e30447e7943fb74015362e29da19c78c
duyue6002 <duyue6002@gmail.com>
2018-07-22 16:05:09 +0800

    add menu

---
+++ src/components/layouts.vue
@@ -0,0 +1,24 @@
+<template>
+  <section class="container" >
+    <section class="menu">
+      <menus></menus>
+    </section>
+    <section class="content-container" >
+      <todo></todo>
+    </section>
+  </section>
+</template>
+
+<script>
+import menus from './menus';
+
+export default {
+  components: {
+    menus,
+  },
+};
+</script>
+
+<style lang="less">
+@import '../common/style/layouts.less';
+</style>
---
+++ src/components/menus.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="list-todos">
+    <a class="list-todo activeListClass list" v-for="item in items">
+      <span class="icon-lock" v-if="item.locked"></span>
+      <span class="count-list" v-if="item.count > 0">{{item.count}}</span>
+      {{item.title}}
+    </a>
+    <a class=" link-list-new">
+      <span class="icon-plus"></span>
+      Add New
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Timothée',
+          count: 1,
+          locked: true,
+        },
+        {
+          title: 'Saoirse',
+          count: 2,
+          locked: true,
+        },
+        {
+          title: 'Robbie',
+          count: 3,
+          locked: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="less">
+@import '../common/style/menu.less';
+</style>
